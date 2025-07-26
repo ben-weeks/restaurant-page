2aec110d9ec0504d69e4c57c7c1709c487f059f1
Ben Weeks <ben.weeks130@gmail.com>
2025-07-25 21:13:37 -0400

    a little work on the homepage

--- src/content/home.js
+++ src/content/home.js
@@ -1,14 +1,25 @@
+// image imports
+import pastel_de_nata from "../images/pastel-de-nata.jpg"
+
 const homePage = (function() {
     // create the main div
     const mainDiv = document.createElement("div")
     mainDiv.classList.add("home")
 
+    let hero = document.createElement("div")
+    hero.classList.add("home-hero")
+
     let heroImage = document.createElement("img")
-    heroImage.src = "../images/pastel-de-nata.jpg"
+    heroImage.src = pastel_de_nata
     heroImage.alt = "Pastel de Nata"
     heroImage.classList.add("home-hero-image")
 
-    mainDiv.appendChild(heroImage)
+    let heroContent = document.createElement("div")
+    heroContent.classList.add("home-hero-content")
+
+    hero.appendChild(heroImage)
+    hero.appendChild(heroContent)
+    mainDiv.appendChild(hero)
 
     return { mainDiv }
 })()
