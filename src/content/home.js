50a340861bf6d1d9b59b653a480809ec87abc1e4
Ben Weeks <ben.weeks130@gmail.com>
2025-07-26 13:39:43 -0400

    refactored to import html segments instead of having to write all elements in JS directly; added menu preview content on homepage

--- src/content/home.js
+++ src/content/home.js
@@ -1,25 +1,13 @@
 // image imports
-import pastel_de_nata from "../images/pastel-de-nata.jpg"
+// import pastel_de_nata from "../images/pastel-de-nata.jpg"
+
+// html import
+import homeContent from "./home.html"
 
 const homePage = (function() {
-    // create the main div
     const mainDiv = document.createElement("div")
     mainDiv.classList.add("home")
-
-    let hero = document.createElement("div")
-    hero.classList.add("home-hero")
-
-    let heroImage = document.createElement("img")
-    heroImage.src = pastel_de_nata
-    heroImage.alt = "Pastel de Nata"
-    heroImage.classList.add("home-hero-image")
-
-    let heroContent = document.createElement("div")
-    heroContent.classList.add("home-hero-content")
-
-    hero.appendChild(heroImage)
-    hero.appendChild(heroContent)
-    mainDiv.appendChild(hero)
+    mainDiv.innerHTML = homeContent
 
     return { mainDiv }
 })()
